89e271932680cb8194e513e678832ff05ecc177f
Yusuf Selim Akdemir <yusakru@gmail.com>
2024-11-11 22:02:28 +0300

    jj

--- Startup.cs
+++ Startup.cs
@@ -35,16 +35,29 @@ namespace OnlineBerberRandevuSistemi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnlineBerberRandevuSistemi v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
+
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnlineBerberRandevuSistemi v1");
             });
         }
     }
